a6e35aecef52ac0bcd59c41f8a2d63d9e4b7953e
asaaev <niaava1@gmail.com>
2023-08-21 18:40:59 +0300

    Initial commit

---
+++ src/main/java/com/asaev/restsensor/PetRestAppSensor/PetRestAppSensorApplication.java
@@ -0,0 +1,13 @@
+package com.asaev.restsensor.PetRestAppSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetRestAppSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetRestAppSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asaev/restsensor/PetRestAppSensor/PetRestAppSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asaev.restsensor.PetRestAppSensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetRestAppSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
